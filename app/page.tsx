86d6e0d538d65bf4db0e63f8fb5bff76528d249b
Prateek Kumar <prateekkum990@gmail.com>
2026-01-05 01:42:03 +0530

    add a basic not found page and basic sidebar with icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center items-center">
+    <main className="flex flex-col justify-center items-center max-w-full">
       <h1>Welcome to NextDoX</h1>
       <p>
         Your one stop solution to project management and planning for solo devs
---
+++ lib/side-bar.ts
@@ -0,0 +1,30 @@
+import { Palette, Home, UserRound, ClipboardPen, Settings } from "lucide-react";
+
+// Menu items.
+export const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Profile",
+    url: "/profile",
+    icon: UserRound,
+  },
+  {
+    title: "Themes",
+    url: "/theme",
+    icon: Palette,
+  },
+  {
+    title: "Canvas",
+    url: "/canvas",
+    icon: ClipboardPen,
+  },
+  {
+    title: "Settings",
+    url: "/settings",
+    icon: Settings,
+  },
+];
